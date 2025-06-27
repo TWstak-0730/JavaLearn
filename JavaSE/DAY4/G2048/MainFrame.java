a7a491dcc06ea9aa1d281782f280b36a6b932d8e
TWstak-0730 <twstak@mail.nwpu.edu.cn>
2025-06-27 22:46:37 +0800

    2048完成JAVASE结束

--- JavaSE/DAY4/G2048/MainFrame.java
+++ JavaSE/DAY4/G2048/MainFrame.java
@@ -1,23 +1,37 @@
 package DAY4.G2048;
 
-import java.awt.HeadlessException;
+import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class MainFrame extends JFrame implements KeyListener{
+public class MainFrame extends JFrame implements KeyListener,ActionListener{
     // int[][] datas = {
     //     {2,4,8,16},
     //     {4,8,16,32},
     //     {8,16,32,64},
     //     {16,32,64,2}
     // };
-    int[][] datas = new int[4][4];
+    JMenuBar menuBar = new JMenuBar();
+
+        JMenu menu1 = new JMenu("换肤");
+        JMenu menu2 = new JMenu("关于我们");
+        JMenu menu3 = new JMenu("功能");
+
+        JMenuItem item1 = new JMenuItem("经典");
+        JMenuItem item2 = new JMenuItem("科技");
+        JMenuItem item3 = new JMenuItem("简约");
+        JMenuItem func1 = new JMenuItem("重新开始");
+    private int score = 0;
+    private int[][] datas = new int[4][4];
+    private boolean WinFlag = false;
+    private boolean LoseFlag = false;
     public MainFrame() throws HeadlessException {
         initFrame();
+        initMenu();
+        GameReset();
         ShowGame();
         setVisible(true);
-
     }
     
     /**
@@ -31,13 +45,21 @@ public class MainFrame extends JFrame implements KeyListener{
         setDefaultCloseOperation(3);
         setAlwaysOnTop(true);
         this.addKeyListener(this);
-        
+
     }
     /**
      * 此方法用于绘制游戏界面
     */
     public void ShowGame(){
         getContentPane().removeAll();
+
+        if(LoseFlag){
+            JLabel losepic = new JLabel(new ImageIcon("C:\\Users\\STAK\\Desktop\\JavaLearn\\2048\\gameOver.png"));
+            losepic.setBounds(90, 100, 334, 228);
+            getContentPane().add(losepic);
+        }
+
+
 
         for(int i=0;i<4;i++){
             for(int j=0;j<4;j++){
@@ -53,10 +75,50 @@ public class MainFrame extends JFrame implements KeyListener{
         JLabel bg = new JLabel(new ImageIcon("C:\\Users\\STAK\\Desktop\\JavaLearn\\2048\\background.png"));
         bg.setBounds(40,40,420,420);
         getContentPane().add(bg);
+        
+
+        JLabel scorelabel = new JLabel("得分："+score);
+        scorelabel.setBounds(50, 20, 100, 20);
+        getContentPane().add(scorelabel);
 
         getContentPane().repaint();
     }
 
+    /**
+     * 此方法用于初始化菜单
+    */
+    public void initMenu(){
+        menu1.add(item1);
+        menu1.add(item2);
+        menu1.add(item3);
+        menu3.add(func1);
+        menuBar.add(menu1);
+        menuBar.add(menu2);
+        menuBar.add(menu3);
+        setJMenuBar(menuBar);
+        item1.addActionListener(this);
+        item2.addActionListener(this);
+        item3.addActionListener(this);
+        func1.addActionListener(this);
+        menu2.addActionListener(this);
+    }
+
+    /**
+     * 此方法用于游戏重置
+    */
+    public void  GameReset(){
+        score = 0;
+        WinFlag = false;
+        LoseFlag = false;
+        
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                datas[i][j] = 0;
+            }
+        }
+        
+        GenNewNum();
+    }
      @Override
     public void keyPressed(KeyEvent e) {
         //System.out.println("键盘按下了");
@@ -92,6 +154,15 @@ public class MainFrame extends JFrame implements KeyListener{
             Lose();
         }
         ShowGame();
+        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    void printTable(){
         for(int i=0;i<4;i++){
             for(int j=0;j<4;j++){
                 System.out.printf("%2d ",datas[i][j]);
@@ -100,12 +171,6 @@ public class MainFrame extends JFrame implements KeyListener{
         System.out.println("");}
         System.out.println();
     }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        //throw new UnsupportedOperationException("Not supported yet.");
-    }
-
     void LeftMove(){
         Stack<Integer> st = new Stack<>();
         Stack<Integer> tmp = new Stack<>();
@@ -121,6 +186,7 @@ public class MainFrame extends JFrame implements KeyListener{
                         else if(flag){
                             st.push(st.pop()+datas[i][j]);
                             flag = false;
+                            score += 10;
                         }else{
                             st.push(datas[i][j]);
                             flag = true;
@@ -160,6 +226,7 @@ public class MainFrame extends JFrame implements KeyListener{
                         else if(flag){
                             st.push(st.pop()+datas[i][j]);
                             flag = false;
+                            score += 10;
                         }else{
                             st.push(datas[i][j]);
                             flag = true;
@@ -199,6 +266,7 @@ public class MainFrame extends JFrame implements KeyListener{
                         else if(flag){
                             st.push(st.pop()+datas[j][i]);
                             flag = false;
+                            score += 10;
                         }else{
                             st.push(datas[j][i]);
                             flag = true;
@@ -237,6 +305,7 @@ public class MainFrame extends JFrame implements KeyListener{
                         else if(flag){
                             st.push(st.pop()+datas[j][i]);
                             flag = false;
+                            score += 10;
                         }else{
                             st.push(datas[j][i]);
                             flag = true;
@@ -260,14 +329,17 @@ public class MainFrame extends JFrame implements KeyListener{
 
     }
     boolean CheckWin(){
+        WinFlag = true;
         for(int i=0;i<4;i++){
             for(int j=0;j<4;j++){
                 if(datas[i][j]==2048){return true;}
             }
         }
+        WinFlag = false;
         return false;
     }
     boolean CheckLose(){
+        LoseFlag = false;
        int[][] cp = new int[4][4];
        copy(datas, cp);
         UpMove();
@@ -281,7 +353,7 @@ public class MainFrame extends JFrame implements KeyListener{
         
         LeftMove();
         if(!IsSame(datas, cp)){copy(cp, datas);return false;}
-        
+        LoseFlag = true;
         return true;
     }
     void copy(int[][] src,int[][] des){
@@ -327,5 +399,23 @@ public class MainFrame extends JFrame implements KeyListener{
             }
         }
         return true;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        //System.out.println(1d);
+        if(e.getSource() == func1){
+            //System.out.println(111);
+            GameReset();
+            ShowGame();
+        } else if(e.getSource() == item1){
+
+        } else if(e.getSource() == item2){
+            
+        } else if(e.getSource() == item3){
+            
+        } else if(e.getSource() == menu2){
+            
+        }
     }
 }
