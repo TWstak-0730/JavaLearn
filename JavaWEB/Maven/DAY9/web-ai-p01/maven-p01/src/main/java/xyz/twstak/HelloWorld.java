e5da4fdb8afe4f3f216b6d1167158708dc7c151c
TWstak-0730 <twstak@mail.nwpu.edu.cn>
2025-07-08 03:17:58 +0800

    DAY9

--- JavaWEB/Maven/DAY9/web-ai-p01/maven-p01/src/main/java/xyz/twstak/HelloWorld.java
+++ JavaWEB/Maven/DAY9/web-ai-p01/maven-p01/src/main/java/xyz/twstak/HelloWorld.java
@@ -1,4 +1,11 @@
 package xyz.twstak;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java application.");
+        System.out.println("It demonstrates the basic structure of a Java program.");
+        System.out.println("You can run this application using Maven or any Java IDE.");
+        System.out.println("Happy coding!");
+    }
 }
